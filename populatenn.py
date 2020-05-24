9ab59618e916beb685412d055d2bab0411436104
Anshul Gupta <ansg191@yahoo.com>
2020-05-24 00:02:18 -0700

    Edited nn

--- populatenn.py
+++ populatenn.py
@@ -2,8 +2,8 @@ import nnData as nn
 from datetime import datetime
 from glob import glob
 
-# nn_list = glob("assets/nn/*.csv")
-# start = max([int(x[19:-4]) for x in nn_list])
+nn_list = glob("assets/nn/*.csv")
+start = max([int(x[19:-4]) for x in nn_list])
 
-for i in range(142, (datetime.today() - datetime(2019, 12, 31)).days + 1):
+for i in range(start, (datetime.today() - datetime(2019, 12, 31)).days + 1):
     nn.pred_to_file2(i, extra_days=50, minimum=100)
